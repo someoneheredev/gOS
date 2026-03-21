dd1f79c17893e2f07de7926a54b368005d26384a
someoneheredev <someoneheredev@gmail.com>
2026-03-22 03:38:43 +1000

    initial committtt

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -4,6 +4,39 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
+
+;
+; FAT12 header
+;
+jmp short start
+nop
+
+bdb_oem:					db 'MSWIN4.1' ; 8 bytes
+bdb_bytes_per_sector: 		dw 512
+bdb_sectors_per_cluster:	db 1
+bdb_reserved_sectors:		dw 1
+bdb_fat_count:				db 2
+bdb_dir_entries_count:		dw 0E0h
+bdb_total_sectors:			dw 2880 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type: 	db 0F0h
+bdb_sectors_per_fat:		dw 9
+bdb_sectors_per_track:		dw 18
+bdb_heads:					dw 2
+bdb_hidden_sectors:			dd 0
+bdb_large_sector_count:		dd 0
+
+; extended boot record
+ebr_drive_number:			db 0 ; 0x00 floppy, 0x80 hdd, useless
+							db 0 ; reserved
+ebr_signature:				db 29h
+ebr_volume_id:				db 12h, 34h, 56h, 78h ; serial number value dont matter
+ebr_volume_label:			db 'gOS        '
+ebr_system_id:				db 'FAT12   '
+
+;
+; Code goes here
+;
+
 start:
 	jmp main
 
@@ -44,18 +77,151 @@ main:
 	mov ss, ax
 	mov sp, 0x7C00 			; stack grows downwards from where we are loaded in memory
 
+	; read something from floppy disk
+	; bios should set dl to drive number
+	mov [ebr_drive_number], dl
+
+	mov ax, 1				; lba=1, second sector from disk
+	mov cl, 1 				; 1 sector to read
+	mov bx, 0x7E00			; data should be after bootloader
+	call disk_read
+
 	; print message
 	mov si, msg_hello
 	call puts
 
+	cli						; disable interrupts
+	hlt
 
+
+;
+;
+; Error handlers
+;
+;
+
+floppy_error:
+	mov si, msg_read_failed
+	call puts
+	jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+	mov ah, 0
+	int 16h					; wait for keypresssssssssssss
+	jmp 0FFFFh:0			; jump to start of bios
 	hlt
 
 .halt:
-	jmp .halt
+	cli						; disable interrupts
+	hlt
 
+
+;
+; Disk Routines
+;
+
+;
+; Converts an LBAS address to a CHS address
+; Parameters:
+; 	- ax: LBA Address
+; Returns:
+; - cx [bits 0-5] sector number
+; - cx [bits 6-15] cylinder
+; - dh: head
+;
+lba_to_chs:
+
+	push ax
+	push dx
+
+	xor dx, dx 									; dx = 0
+	div word [bdb_sectors_per_track] 			; ax = LBA / SectorsPerTrack
+	 											; dx = LBA % SectorsPerTrack
+
+	inc dx										; dx = (LBA & SectorsPerTrack + 1) = sector
+	mov cx, dx
+
+	xor dx, dx									; dx = 0
+	div word [bdb_heads]						; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+												; dx = (LBA / SectorsPerTrack) % Heads = head
+	mov dh, dl									; dh = head
+	mov ch, al									; ch = cylinder (lower 8 bits)
+	shl ah, 6
+	or cl, ah 									; put upper 2 bits of cylinder in CL
+
+	pop ax
+	mov dl, al									; restore DL
+	pop ax
+	ret
+
+;
+; Reads sectors from a disk
+; Parameters:
+;	- ax: LBA address
+;	- cl: number of sectors to read (up to 128)
+;	- dl: drive number
+;	- ex:bx: memory address where to store read data
+;
+disk_read:
+
+	push ax										; save registers we will modify
+	push bx
+	push cx
+	push dx
+	push di
+
+	push cx										; temporarily save CL (number of sec to read)
+	call lba_to_chs								; compute CHS
+	pop ax										; AL = number of sec to read
+	
+	mov ah, 02h
+	mov di, 3									; retry count
+
+.retry:
+	pusha										; save all registers, we don't know what bios modifies
+	stc 										; set carry flag, some bios dont do ts
+	int 13h										; carry flag cleared = work
+	jnc .done									; jump if carry not set
+
+	; read fail
+	popa
+	call disk_reset
+
+	dec di
+	test di, di
+	jnz .retry
+
+.fail:
+	; after all attempts are gone
+	jmp floppy_error
+
+.done:
+	popa
+
+	pop di
+	pop dx
+	pop cx
+	pop bx
+	pop ax										; restore them
+	ret
+
+;
+; Resets disk controller
+; Paramaters:
+;	dl: drive number
+;
+
+disk_reset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc floppy_error
+	popa
+	ret
 
 msg_hello: db 'Hello world!', ENDL, 0
+msg_read_failed: db 'Failed to read floppy disk!', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
